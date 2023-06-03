52f24645f884364421efe3c37c2449134e2a9fc8
guilhermerabeloo <guilhermerabelo699@gmail.com>
2023-06-03 11:34:22 -0300

    Feat/Configurando estutura de roteamento da pagina de erro

--- web/src/app/main.jsx
+++ web/src/app/main.jsx
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import Produtos from './produtos/Produtos.jsx'
+import ErrorPage from '../components/ErrorPage.jsx'
 import App from './App.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: '/produtos',
---
+++ web/src/components/ErrorPage.jsx
@@ -0,0 +1,7 @@
+export default function ErrorPage() {
+    return (
+        <div>
+            <p>Erro 404!</p>
+        </div>
+    )
+}
