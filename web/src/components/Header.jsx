688f463ac987e5a47c023d8b3867fab0a276c578
guilhermerabeloo <guilhermerabelo699@gmail.com>
2023-06-03 11:45:14 -0300

    Feat/Criando funcionalidade de navegar para pagina produtos ou home

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Logo from '../assets/img/Logo.png';
 import user from '../assets/img/user.jpeg';
 import { BsSearch } from "react-icons/bs";
@@ -6,7 +7,7 @@ import { BsFillGearFill } from "react-icons/bs";
 export function Header() {
     return (
         <div id="header">
-            <div className="header-itens" id="logo"><img src={Logo} alt="Logo da empresa" /></div>
+            <div className="header-itens" id="logo"><Link to={'/'}><img src={Logo} alt="Logo da empresa" /></Link></div>
             <div className="header-itens" id="pesquisa"><input type="text" placeholder="Pesquisar" /><button><BsSearch /></button></div>
             <div className="header-itens" id="config"><BsFillGearFill /></div>
             <div className="header-itens" id="user-name"><p>Olá, Guilherme Rabelo</p></div>
